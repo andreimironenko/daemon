2655fab9c00dd8d5feec0ca7a5556e16eb062425
Andrei Mironenko <andrei.mironenko@paralleldynamic.com>
2020-11-02 09:43:59 +0000

    Adding MQ to the daemon

--- include/daemon.h
+++ include/daemon.h
@@ -3,11 +3,27 @@
 //
 
 #pragma once
-
+#include <memory>
+#include <string>
 namespace daemon {
+    // Forward declarations;
     class core {
 
-    };
+    class core_;
+    std::unique_ptr<core_> _impl;
 
+    public:
+        explicit core(std::string name);
+
+        virtual ~core() = default;
+        core(core&&) = delete;
+        core(const core&) = delete;
+        core& operator=(core&&) = delete;
+        core& operator=(const core&) = delete;
+        void start();
+        void stop();
+        void restart();
+        std::string status();
+    };
 } //namespace daemon
 
